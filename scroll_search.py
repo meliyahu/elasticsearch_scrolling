556703d201cc7bb74421102e46a95a1ba2ee17f0
Mosheh EliYahu <mosheh.eliyahu@adelaide.edu.au>
2019-12-10 17:02:36 +1030

    Feat: minor edits

--- scroll_search.py
+++ scroll_search.py
@@ -2,21 +2,25 @@ from elasticsearch_dsl import Search, Q
 from elasticsearch import Elasticsearch, helpers
 import logging
 import json
+import os
 
 # Define config
-host = "http://locahost:9200"
+host = os.environ.get('ES_HOST', "http://locahost:9200")
 port = 80
 timeout = 1000
-index = "site_tax_strata"
+index = "corveg_bioregions"
 doc_type = "doc"
-size = 1000
+size = 5
 body = {}
 
 # Init Elasticsearch instance
 es = Elasticsearch(hosts=[host], timeout=timeout)
 
 # Process hits here
+
+
 def process_hits(hits):
+    print(f' hits = {len(hits)}')
     for item in hits:
         print(json.dumps(item, indent=2))
 
@@ -24,6 +28,7 @@ def process_hits(hits):
 # if not es.indices.exists(index=index):
 #     print("Index " + index + " not exists")
 #     exit()
+
 
 # Init scroll by search
 data = es.search(
@@ -37,12 +42,13 @@ data = es.search(
 sid = data['_scroll_id']
 scroll_size = len(data['hits']['hits'])
 total = scroll_size
+
 while scroll_size > 0:
     "Scrolling..."
-    
+
     # Before scroll, process current batch of hits
     process_hits(data['hits']['hits'])
-    
+
     data = es.scroll(scroll_id=sid, scroll='2m')
 
     # Update the scroll ID
@@ -51,4 +57,5 @@ while scroll_size > 0:
     # Get the number of results that returned in the last scroll
     scroll_size = len(data['hits']['hits'])
     total += scroll_size
-print(f'total documents = {total}')
+
+print(f'total is { total }')
